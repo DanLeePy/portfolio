c46d6bf210fc2f5a771bd4d7dd4b4d21a07e8128
daniel <danielleeweisheng@gmail.com>
2020-05-25 11:37:38 +0800

    Updates

---
+++ precache-manifest.db621f60045b4ef888de7d801a9b4e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961a8a2a9a0a84935bc7f2e64bed1553",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "611913c8e11d86db86f2",
+    "url": "/portfolio/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "1e8cd8db58eb4b539ce1",
+    "url": "/portfolio/static/js/2.3967d565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3967d565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611913c8e11d86db86f2",
+    "url": "/portfolio/static/js/main.062b846c.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
